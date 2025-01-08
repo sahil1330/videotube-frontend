8564ee95091c401ebffb0174e72e040deb76846b
sahil1330 <sahilmane025@gmail.com>
2025-01-09 00:30:08 +0530

    feat: improve accessibility by adding alt text to logo; refactor WatchVideo component layout

--- src/pages/WatchVideo.tsx
+++ src/pages/WatchVideo.tsx
@@ -1,9 +1,11 @@
 function WatchVideo() {
     return (
         <>
-            <div className="video-player">
-                <div className="aspect-video rounded-xl bg-muted/50" >
-                    <video src="https://www.w3schools.com/html/mov_bbb.mp4" controls></video>
+            <div className="wrapper w-5/6 flex mx-auto">
+                <div className="video-player flex mx-auto ">
+                    <div className="aspect-video rounded-xl bg-muted/50 w-1/2" >
+                        {/* <video src="https://www.w3schools.com/html/mov_bbb.mp4" className="w-full" controls></video> */}
+                    </div>
                 </div>
             </div>
         </>
