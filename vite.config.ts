e5927626dd85de4771c883e55e939734fb898609
sahil1330 <sahilmane025@gmail.com>
2025-04-28 01:49:56 +0530

    refactor: simplify Vite configuration by removing environment variable loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,21 @@
 import path from "node:path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vite.dev/config/
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const API_URL = `${env.VITE_API_URL ?? "http://localhost:8000"}`;
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          target: API_URL,
-          changeOrigin: true,
-        },
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://videotube-gm5j2.ondigitalocean.app",
+        changeOrigin: true,
       },
     },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
+  },
 });
