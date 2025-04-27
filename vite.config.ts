8aba772478b4d3813c303a4b810844e5b7deacae
sahil1330 <sahilmane025@gmail.com>
2025-04-28 00:26:01 +0530

    feat: update environment configuration; remove sensitive data from .env and add API_URL to .env.sample

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const API_URL = `${env.VITE_API_URL ?? "http://localhost:8000"}`;
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
