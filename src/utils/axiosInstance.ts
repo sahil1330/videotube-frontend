75b6bafb7324287c2bd0c70e514a35f6aa543cfd
sahil1330 <sahilmane025@gmail.com>
2025-04-28 10:07:18 +0530

    fix: update axios instance base URL to use environment variable

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://videotube-gm5j2.ondigitalocean.app/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
   withCredentials: true,
-  proxy: {
-    host: import.meta.env.VITE_API_HOSTNAME,
-    port: 443,
-  },
 });
 
 export default axiosInstance;
